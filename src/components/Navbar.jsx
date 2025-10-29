1fbf508dbbe881432f42894d6a5e3b2c523cd5cd
NSBTEK <NSBTEK@GMAIL.COM>
2025-10-29 18:01:06 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,18 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="bg-teal-600 text-white p-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold">NSBTEK Solutions</h1>
+    <nav className="bg-teal-600 text-white p-4 flex justify-between items-center shadow-md">
+      {/* Logo */}
+      <Link to="/">
+        <img src="/logo.png" alt="Logo" className="h-10 w-auto" />
+      </Link>
+
+      {/* Navigation links */}
       <div className="space-x-4">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/contact">Contact</Link>
+        <Link to="/" className="hover:underline">Home</Link>
+        <Link to="/about" className="hover:underline">About</Link>
+        <Link to="/services" className="hover:underline">Services</Link>
+        <Link to="/contact" className="hover:underline">Contact</Link>
       </div>
     </nav>
   )
